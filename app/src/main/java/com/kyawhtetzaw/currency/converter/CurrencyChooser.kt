31b9df9df1e7eec828580138da5e2dcabca8cc72
xinkev <lyx13710@gmail.com>
2023-02-20 16:13:20 +0630

    Improve CurrencyChooser UI Add a title and a cancel button for better UX

--- app/src/main/java/com/kyawhtetzaw/currency/converter/CurrencyChooser.kt
+++ app/src/main/java/com/kyawhtetzaw/currency/converter/CurrencyChooser.kt
@@ -1,8 +1,9 @@
 package com.kyawhtetzaw.currency.converter
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,6 +13,7 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
@@ -26,6 +28,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.kyawhtetzaw.currency.R
+import com.kyawhtetzaw.currency.R.string
 import com.kyawhtetzaw.currency.model.ExchangeRate
 
 @Composable
@@ -55,6 +58,7 @@ fun CurrencyChooser(
         )
     }
 }
+
 @Composable
 private fun CurrencyChooserDialog(
     show: Boolean,
@@ -69,7 +73,8 @@ private fun CurrencyChooserDialog(
                 onSelect = {
                     onSelect(it)
                     onDismissRequest()
-                }
+                },
+                onCancelClick = onDismissRequest
             )
         }
     }
@@ -78,27 +83,42 @@ private fun CurrencyChooserDialog(
 @Composable
 private fun CurrencyChooserDialogLayout(
     currencies: List<ExchangeRate>,
-    onSelect: (ExchangeRate) -> Unit
+    onSelect: (ExchangeRate) -> Unit,
+    onCancelClick: () -> Unit
 ) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        LazyColumn {
-            itemsIndexed(
-                items = currencies,
-                key = { _, currency -> currency.symbol }) { i, currency ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable(onClick = { onSelect(currency) }),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = currency.symbol,
-                        modifier = Modifier.padding(16.dp)
-                    )
+    Surface(Modifier.fillMaxHeight(.9f)) {
+        Column {
+            Text(
+                text = stringResource(id = string.choose_a_currency),
+                modifier = Modifier.padding(16.dp)
+            )
+            LazyColumn(Modifier.weight(1f)) {
+                itemsIndexed(
+                    items = currencies,
+                    key = { _, currency -> currency.symbol }) { i, currency ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable(onClick = { onSelect(currency) }),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = currency.symbol,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
+                    if (i != currencies.lastIndex) {
+                        Divider()
+                    }
                 }
-                if (i != currencies.lastIndex) {
-                    Divider()
-                }
+            }
+            TextButton(
+                onClick = onCancelClick,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .align(Alignment.End)
+            ) {
+                Text(text = stringResource(R.string.cancel))
             }
         }
     }
@@ -109,5 +129,5 @@ private fun CurrencyChooserDialogLayout(
 private fun CurrencyChooserPreview() {
     val currencies = listOf(ExchangeRate("USD", 1.0), ExchangeRate("HKD", 11.0))
 
-    CurrencyChooserDialogLayout(currencies = currencies, onSelect = {})
+    CurrencyChooserDialogLayout(currencies = currencies, onSelect = {}, onCancelClick = {})
 }

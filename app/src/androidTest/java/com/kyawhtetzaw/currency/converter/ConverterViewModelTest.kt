da5eb4c90fd1180f1010ceb9973eccdf47253d2d
xinkev <lyx13710@gmail.com>
2023-02-21 02:25:54 +0630

    Removed ./idea folder

---
+++ app/src/androidTest/java/com/kyawhtetzaw/currency/converter/ConverterViewModelTest.kt
@@ -0,0 +1,10 @@
+package com.kyawhtetzaw.currency.converter
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import org.junit.Rule
+
+internal class  ConverterScreenTest {
+    @get:Rule
+    val cr = createAndroidComposeRule<ComponentActivity>()
+}
